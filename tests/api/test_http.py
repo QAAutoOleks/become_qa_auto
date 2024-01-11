926850d894eb1d1a560c8c42a6a6cf26e3ad2b27
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-01-11 21:22:13 +0200

    modified:   tests/api/__pycache__/test_http.cpython-312-pytest-7.4.3.pyc modified:   tests/api/test_http.py

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -11,6 +11,13 @@ def test_first_request():
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
     # print(f"Response is {r.text}")
-    print(f"Response Body is {r.json()}")
-    print(f"Response Status code is {r.status_code}")
-    print(f"Response Headers are {r.headers}")
+    # print(f"Response Body is {r.json()}")
+    # print(f"Response Status code is {r.status_code}")
+    # print(f"Response Headers are {r.headers}")
+    
+    url = r.json()
+    headers = r.headers
+
+    assert url['organizations_url'] == 'https://api.github.com/users/defunkt/orgs'
+    assert r.status_code == 200
+    assert headers['Content-Security-Policy'] ==  "default-src 'none'"
