e152588f19abdb1babe3699719891d6cff553d0c
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-01-12 11:44:22 +0200

    modified:   pytest.ini modified:   tests/api/__pycache__/test_http.cpython-312-pytest-7.4.3.pyc modified:   tests/api/test_http.py

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -7,6 +7,7 @@ def test_first_request():
     r = requests.get('https://api.github.com/zen')
     print(r.text)
 
+
 @pytest.mark.http
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
@@ -14,10 +15,42 @@ def test_second_request():
     # print(f"Response Body is {r.json()}")
     # print(f"Response Status code is {r.status_code}")
     # print(f"Response Headers are {r.headers}")
-    
+
     url = r.json()
     headers = r.headers
 
     assert url['organizations_url'] == 'https://api.github.com/users/defunkt/orgs'
     assert r.status_code == 200
-    assert headers['Content-Security-Policy'] ==  "default-src 'none'"
+    assert headers['Content-Security-Policy'] == "default-src 'none'"
+
+
+@pytest.mark.swagger_petstore
+def test_swagger():
+    base_url = 'https://petstore.swagger.io/v2/swagger.json'
+    r = requests.get(base_url)
+
+    add_pet = {
+        "id": 1,
+        "category": {
+            "id": 1,
+            "name": "Pes"
+        },
+        "name": "doggie",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    }
+    r_post = requests.post('https://petstore.swagger.io/v2/pet', json=add_pet)
+    r_get = requests.get('https://petstore.swagger.io/v2/pet/1')
+    r_get_json = r_get.json()
+    print(r_get_json)
+
+    assert r.status_code == 200
+    assert r_post.status_code == 200
