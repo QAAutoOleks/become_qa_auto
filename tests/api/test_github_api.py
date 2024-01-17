418610513aa7237ed4a97b00914a22c835bbacb1
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-01-17 19:39:56 +0200

    modified:   modules/api/clients/__pycache__/github.cpython-312.pyc modified:   modules/api/clients/github.py modified:   tests/api/__pycache__/test_github_api.cpython-312-pytest-7.4.3.pyc modified:   tests/api/test_github_api.py

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -10,3 +10,14 @@ def test_user_exists(github_api):
 def test_user_not_exists(github_api):
     user = github_api.get_user('seergiybutenko')
     assert user['message'] == 'Not Found'
+
+@pytest.mark.api_git
+def test_repo_can_be_found(github_api):
+    repo = github_api.search_repo('become-qa-auto')
+    assert repo['total_count'] == 54
+    assert 'become-qa-auto' in repo['items'][0]['name']
+
+@pytest.mark.api_git
+def test_repo_cannot_be_found(github_api):
+    repo = github_api.search_repo('become-qa-auto_oleksandr')
+    assert repo['total_count'] == 0
