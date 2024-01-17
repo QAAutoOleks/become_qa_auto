37d7202eec199d4e2c8a3977a5bf8b86221348ed
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-01-17 19:03:30 +0200

    modified:   __pycache__/conftest.cpython-312-pytest-7.4.3.pyc modified:   conftest.py modified:   tests/api/__pycache__/test_github_api.cpython-312-pytest-7.4.3.pyc modified:   tests/api/test_github_api.py

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -1,15 +1,12 @@
 import pytest
-from modules.api.clients.github import GitHub
 
 
 @pytest.mark.api_git
-def test_user_exists():
-    api = GitHub()
-    user = api.get_user('defunkt')
+def test_user_exists(github_api):
+    user = github_api.get_user('defunkt')
     assert user['login'] == 'defunkt'
 
 @pytest.mark.api_git
-def test_user_not_exists():
-    api = GitHub()
-    user = api.get_user('seergiybutenko')
+def test_user_not_exists(github_api):
+    user = github_api.get_user('seergiybutenko')
     assert user['message'] == 'Not Found'
