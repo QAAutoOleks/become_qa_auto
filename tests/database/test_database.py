ddc0b6d3dae61fc2f72b3d26f0954e2d351a1106
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-01-26 16:20:09 +0200

    modified:   become_qa_auto.db modified:   tests/database/__pycache__/test_database.cpython-311-pytest-7.4.4.pyc modified:   tests/database/test_database.py

--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -1,80 +1,78 @@
 import pytest
 import sqlite3
-from sqlite3 import OperationalError
 from datetime import date
 from modules.common.database import Database
-from sqlite3 import DatabaseError
 
 
-# @pytest.mark.database
-# def test_database_connection():
-#     db = Database()
-#     db.testing_connection()
+@pytest.mark.database
+def test_database_connection():
+    db = Database()
+    db.testing_connection()
 
-# @pytest.mark.database
-# def test_check_all_users():
-#     db = Database()
-#     users = db.get_all_users()
+@pytest.mark.database
+def test_check_all_users():
+    db = Database()
+    users = db.get_all_users()
 
-#     print(users)
+    print(users)
 
-# @pytest.mark.database
-# def test_check_user_sergii():
-#     db = Database()
-#     user = db.get_user_adress_by_name('Sergii')
+@pytest.mark.database
+def test_check_user_sergii():
+    db = Database()
+    user = db.get_user_adress_by_name('Sergii')
 
-#     assert user[0][0] == 'Maydan Nezalezhnosti 1'
-#     assert user[0][1] == 'Kyiv'
-#     assert user[0][2] == '3127'
-#     assert user[0][3] == 'Ukraine'
+    assert user[0][0] == 'Maydan Nezalezhnosti 1'
+    assert user[0][1] == 'Kyiv'
+    assert user[0][2] == '3127'
+    assert user[0][3] == 'Ukraine'
 
-# @pytest.mark.database
-# def test_update_quantity_of_products():
-#     db = Database()
-#     db.update_quantity_of_products('з цукром', 25)
+@pytest.mark.database
+def test_update_quantity_of_products():
+    db = Database()
+    db.update_quantity_of_products('з цукром', 25)
 
-#     assert db.get_quantity_products('з цукром')[0][0] == 25
+    assert db.get_quantity_products('з цукром')[0][0] == 25
 
-# @pytest.mark.database
-# def test_create_new_product():
-#     db = Database()
-#     # в результаті отримуємо tuple
-#     assert db.create_new_product(11, 'печиво', 'солодке', 30)[0][0] == 30
+@pytest.mark.database
+def test_create_new_product():
+    db = Database()
+    # в результаті отримуємо tuple
+    assert db.create_new_product(11, 'печиво', 'солодке', 30)[0][0] == 30
 
-# @pytest.mark.database
-# def test_create_and_delete():
-#     db = Database()
-#     db.create_new_product(20, 'test', 'data', 999)
-#     db.delete_product(20)
-#     assert db.get_product_by_id(20) == []
+@pytest.mark.database
+def test_create_and_delete():
+    db = Database()
+    db.create_new_product(20, 'test', 'data', 999)
+    db.delete_product(20)
+    assert db.get_product_by_id(20) == []
 
-# @pytest.mark.database
-# def test_list_of_data_orders():
-#     db = Database()
-#     assert db.get_list_of_data()[0][0] == 1
-#     assert db.get_list_of_data()[0][1] == 1
-#     assert db.get_list_of_data()[0][2] == 'солодка вода'
-#     assert db.get_list_of_data()[0][3] == 'з цукром'
+@pytest.mark.database
+def test_list_of_data_orders():
+    db = Database()
+    assert db.get_list_of_data()[0][0] == 1
+    assert db.get_list_of_data()[0][1] == 1
+    assert db.get_list_of_data()[0][2] == 'солодка вода'
+    assert db.get_list_of_data()[0][3] == 'з цукром'
 
-# @pytest.mark.database
-# def test_insert_data_in_table_orders():
-#     db = Database()
-#     db.insert_in_orders_data()
-#     assert len(db.get_list_of_data_orders()) == 6
-#     assert db.get_list_of_data_orders()[0][4] == str(date.today())
-#     assert len(
-#         db.get_orders_inner_join_customers_and_products_by_name_of_product(
-#             'солодка вода'
-#         )) == 4
-#     assert db.get_orders_inner_join_customers_and_products_by_name_of_product(
-#             'молоко'
-#         )[0] == ('молоко', 1.5, '2024-01-26', 'Sergii', 'натуральне незбиране')
-#     assert (len(db.get_orders_inner_join_customers_and_products_by_name_of_customer(
-#         'Sergii'
-#         ))) == 3
-#     assert (db.get_orders_inner_join_customers_and_products_by_name_of_customer(
-#         'Sergii'
-#         ))[0] == ('Sergii', 'солодка вода', 3.5, '2024-01-26', 'з цукром')
+@pytest.mark.database
+def test_insert_data_in_table_orders():
+    db = Database()
+    db.insert_in_orders_data()
+    assert len(db.get_list_of_data_orders()) == 6
+    assert db.get_list_of_data_orders()[0][4] == str(date.today())
+    assert len(
+        db.get_orders_inner_join_customers_and_products_by_name_of_product(
+            'солодка вода'
+        )) == 4
+    assert db.get_orders_inner_join_customers_and_products_by_name_of_product(
+            'молоко'
+        )[0] == ('молоко', 1.5, '2024-01-26', 'Sergii', 'натуральне незбиране')
+    assert (len(db.get_orders_inner_join_customers_and_products_by_name_of_customer(
+        'Sergii'
+        ))) == 3
+    assert (db.get_orders_inner_join_customers_and_products_by_name_of_customer(
+        'Sergii'
+        ))[0] == ('Sergii', 'солодка вода', 3.5, '2024-01-26', 'з цукром')
 
 @pytest.mark.database
 def test_insert_invalid_data_types():
