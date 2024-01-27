5b2aec26c1515b779b3f20cb3980f35771bdd8c8
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-01-27 13:36:06 +0200

    new file:   modules/ui/page_objects/rozetka_main_page.py

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -6,19 +6,19 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 
-@pytest.mark.ui
-def test_check_incorrect_username():
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install())
-    )
+# @pytest.mark.ui
+# def test_check_incorrect_username():
+#     driver = webdriver.Chrome(
+#         service=Service(ChromeDriverManager().install())
+#     )
 
-    driver.get("https://github.com/login")
-    login_element = driver.find_element(By.ID, 'login_field')
-    login_element.send_keys('wrong.data@mail.com')
-    login_element = driver.find_element(By.ID, 'password')
-    login_element.send_keys('wrong_password')
-    button_element = driver.find_element(By.NAME, 'commit')
-    button_element.click()
-    assert driver.title == 'Sign in to GitHub · GitHub'
+#     driver.get("https://github.com/login")
+#     login_element = driver.find_element(By.ID, 'login_field')
+#     login_element.send_keys('wrong.data@mail.com')
+#     login_element = driver.find_element(By.ID, 'password')
+#     login_element.send_keys('wrong_password')
+#     button_element = driver.find_element(By.NAME, 'commit')
+#     button_element.click()
+#     assert driver.title == 'Sign in to GitHub · GitHub'
 
-    driver.close()
+#     driver.close()
