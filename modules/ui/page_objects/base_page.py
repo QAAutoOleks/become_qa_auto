59b3a577e8511b3a939e1e432e4833c420569643
Oleksandr Tsupko <aleksandr.tsupko@gmail.com>
2024-01-27 14:42:50 +0200

    modified:   modules/ui/page_objects/rozetka_main_page.py

--- modules/ui/page_objects/base_page.py
+++ modules/ui/page_objects/base_page.py
@@ -1,5 +1,4 @@
-import os
-from platform import system
+import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,20 +8,8 @@ from selenium.webdriver.chrome.options import Options
 
 class BasePage:
     def __init__(self):
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        
-        options = Options()
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        exec_path = os.path.join(os.getcwd(), 'driver', 'chromedriver.exe') if system() == "Windows" else \
-            os.path.join(os.getcwd(), 'driver', 'chromedriver')
-        driver = webdriver.Chrome(options=options, service=Service(log_path=os.devnull, executable_path=exec_path))
-        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-        'source': '''
-        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Array;
-        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Promise;
-        delete window.cdc_adoQpoasnfa76pfcZLmcfl_Symbol;
-        '''
-        })
+        #self.driver_service = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver = uc.Chrome()
 
     def close(self):
         self.driver.close()
--- modules/ui/page_objects/rozetka_main_page.py
+++ modules/ui/page_objects/rozetka_main_page.py
@@ -2,7 +2,6 @@ from modules.ui.page_objects.base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 import time
 
 
@@ -15,14 +14,8 @@ class RozetkaMainPage(BasePage):
         self.driver.get(link)
 
     def sales_elements(self):
-        time.sleep(5)
-        try:
-            element = WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located((By.XPATH, '/html/body/app-root/div/div/rz-main-page/div/main/rz-main-page-content/rz-goods-sections/section[1]/rz-goods-section/ul/li[1]/rz-app-tile/div')))
-        finally:
-            pass
-            #driver.quit()
-        # sales_elements = self.driver.find_elements(
-        #     By.CLASS_NAME, 'tile')
-        time.sleep(3)
-        print(len(elements))
+        time.sleep(2)        
+        sales = self.driver.find_elements(
+            By.CSS_SELECTOR, '.tile')
+        for i in sales:
+            print("element:", i)
